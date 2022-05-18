0a333616fa236e12d721d4b7f731f10345b9cd33
veirs <vveirs@coloradocollege.edu>
2022-05-17 18:49:43 -0700

    add write to test.py

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+import os
+print("Current working directory is:", os.cwd())
 lines = ['This is a line', 'this is another line']
 with open('text.txt', 'w') as f:
     f.writelines(lines)
+
+with open ('text.txt', 'r') as f:
+    print(f.readline)
