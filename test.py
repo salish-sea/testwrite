46a307bd7f7dbf3b8f9ca811b9b0f2f764ab235f
veirs <vveirs@coloradocollege.edu>
2022-05-17 20:11:14 -0700

    try ./

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import os
 print("Current working directory is:", os.getcwd())
 lines = ['This is a line', 'this is another line']
-with open('text.txt', 'w') as f:
+with open('./text.txt', 'w') as f:
     f.writelines(lines)
 
-with open ('text.txt', 'r') as f:
+with open ('./text.txt', 'r') as f:
     print(f.readline())
